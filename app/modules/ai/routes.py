223fb0d76e453ebdba8f68fb80c7c697336230d3
franciiscocg <cisquitofrancisco@gmail.com>
2024-11-27 18:36:32 +0000

    feature: Add new question

--- app/modules/ai/routes.py
+++ app/modules/ai/routes.py
@@ -1,22 +1,15 @@
 from flask import request, jsonify, render_template
 from app.modules.ai import ai_bp
 from app.modules.ai.services import AiService
+from app.modules.dataset.services import DataSetService
+from app.modules.featuremodel.services import FeatureModelService
 
 ai_service = AiService()
+dataset_service = DataSetService()
+feature_model_service = FeatureModelService()
 
 project_id = "amazing-source-213816"
 language_code = "es"
-
-
-def get_datasets():
-    # Aquí puedes hacer una consulta a tu base de datos o cualquier otro origen de datos
-    datasets = [
-        "Dataset 1: Usuarios",
-        "Dataset 2: Transacciones",
-        "Dataset 3: Productos",
-        "Dataset 4: Reviews"
-    ]
-    return datasets
 
 
 @ai_bp.route('/chat', methods=['POST'])
@@ -25,9 +18,35 @@ def chat():
            
     response_text = ai_service.detect_intent_texts(project_id, "unique-session-id", user_message, language_code)
     
-    if response_text == 'dataset':
-        datasets = get_datasets()
-        response_text = "Los datasets disponibles son:\n" + "\n".join(datasets)
+    if response_text == 'dataset-numero':
+        datasets_counter = dataset_service.count_synchronized_datasets()
+        response_text = "Los datasets disponibles son\n" + str(datasets_counter)
+        
+    if response_text == 'feature-number':
+        feature_models_counter = feature_model_service.count_feature_models()
+        response_text = "Los features disponibles son\n" + str(feature_models_counter)
+        
+    if response_text == 'dataset-downloads':
+        total_dataset_downloads = dataset_service.total_dataset_downloads()
+        response_text = "El total de descargas de datasets es\n" + str(total_dataset_downloads)
+        
+    if response_text == 'feature-downloads':
+        total_feature_model_downloads = feature_model_service.total_feature_model_downloads()
+        response_text = "El total de descargas de features es\n" + str(total_feature_model_downloads)
+        
+    if response_text == 'dataset-views':
+        total_dataset_views = dataset_service.total_dataset_views()
+        response_text = "El total de visualizaciones de datasets es\n" + str(total_dataset_views)
+        
+    if response_text == 'feature-views':
+        total_feature_model_views = feature_model_service.total_feature_model_views()
+        response_text = "El total de visualizaciones de features es\n" + str(total_feature_model_views)
+        
+    if response_text == 'dataset-latest':
+        response_text = "Los datasets más recientes son\n" + str(dataset_service.latest_synchronized())
+        
+    if response_text == 'feature-latest':
+        response_text = "Los features más recientes son\n" + str(feature_model_service.latest_feature_models())
         
     return jsonify({"response": response_text})
 
