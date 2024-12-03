2753068601b5d0eb3665538fe3f912556fdbe69c
franciiscocg <cisquitofrancisco@gmail.com>
2024-12-03 11:22:43 +0000

    feature: improve with new question

--- app/modules/ai/routes.py
+++ app/modules/ai/routes.py
@@ -12,6 +12,28 @@ project_id = "amazing-source-213816"
 language_code = "es"
 
 
+def format_dataset_response(dataset):
+    authors = "\n".join([f"  - {author['name']} ({author['affiliation']})" for author in dataset['authors']])
+    files = "\n".join([f"  - {file['name']} ({file['size_in_human_format']})" for file in dataset['files']])
+    return (
+        f"El dataset solicitado es:\n"
+        f"  Título: {dataset['title']}\n"
+        f"  ID: {dataset['id']}\n"
+        f"  Creado en: {dataset['created_at']}\n"
+        f"  Descripción: {dataset['description']}\n"
+        f"  Autores:\n{authors}\n"
+        f"  Tipo de publicación: {dataset['publication_type']}\n"
+        f"  DOI de la publicación: {dataset['publication_doi']}\n"
+        f"  DOI del dataset: {dataset['dataset_doi']}\n"
+        f"  Tags: {', '.join(dataset['tags'])}\n"
+        f"  URL: {dataset['url']}\n"
+        f"  Descargar: {dataset['download']}\n"
+        f"  Zenodo: {dataset['zenodo']}\n"
+        f"  Archivos:\n{files}\n"
+        f"  Número de archivos: {dataset['files_count']}\n"
+        f"  Tamaño total: {dataset['total_size_in_human_format']}\n"
+    )
+
 @ai_bp.route('/chat', methods=['POST'])
 def chat():
     user_message = request.json.get('text')
@@ -43,10 +65,26 @@ def chat():
         response_text = "El total de visualizaciones de features es\n" + str(total_feature_model_views)
         
     if response_text == 'dataset-latest':
-        response_text = "Los datasets más recientes son\n" + str(dataset_service.latest_synchronized())
+        latest_datasets = dataset_service.latest_synchronized()
+        dataset_names = [str(dataset.id) for dataset in latest_datasets]  # Convertir a cadenas
+        response_text = "Los datasets más recientes son:\n" + "\n".join(dataset_names)
         
-    if response_text == 'feature-latest':
-        response_text = "Los features más recientes son\n" + str(feature_model_service.latest_feature_models())
+    if response_text.startswith('dataset-get'):
+        dataset_id = response_text.split(',')[-1]
+        dataset = dataset_service.get_by_id(dataset_id)
+        if dataset is None:
+            response_text = "no se encontró el dataset"
+        else:
+            dataset = dataset.to_dict()
+            response_text = format_dataset_response(dataset)
+    
+    if response_text.startswith('feature-get'):
+        feature_model_id = response_text.split(',')[-1]
+        feature_model = feature_model_service.get_by_id(feature_model_id)
+        if feature_model is None:
+            response_text = "no se encontró el feature"
+        else:
+            response_text = feature_model
         
     return jsonify({"response": response_text})
 
